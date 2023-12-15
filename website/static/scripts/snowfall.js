dff77b14880bc5b26c4472db483aac519965e88e
michaellgans <michaellgans@Victoria>
2023-12-15 11:45:53 -0600

    M - updates files to newest version

---
+++ website/static/scripts/snowfall.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const snowfallContainer = document.getElementById("snowfall");
+
+    for (let i = 0; i < 50; i++) {
+        createSnowflake();
+    }
+
+    function createSnowflake() {
+        const snowflake = document.createElement("div");
+        snowflake.className = "snowflake";
+        snowflake.style.left = Math.random() * 100 + "vw";
+        snowflake.style.animationDuration = Math.random() * 2 + 1 + "s";
+        snowflake.style.animationDelay = Math.random() + "s";
+        snowfallContainer.appendChild(snowflake);
+
+        snowflake.addEventListener("animationiteration", function () {
+            snowflake.style.left = Math.random() * 100 + "vw";
+            snowflake.style.animationDuration = Math.random() * 2 + 1 + "s";
+            snowflake.style.animationDelay = Math.random() + "s";
+        });
+    }
+});
