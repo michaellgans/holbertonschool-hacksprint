464275d3b177f5aaa9206791dd4cd8fb54e7f0fa
michaellgans <michaellgans@Victoria>
2023-12-17 14:36:20 -0600

    M - turned snow back on

--- website/static/scripts/cartScript.js
+++ website/static/scripts/cartScript.js
@@ -1,15 +1,23 @@
 /* Wait for document to load */
 $(document).ready(function () {
-    const addToCart = $('.sockName');
+    const addToCart1 = $('#name1');
+    const addToCart2 = $('#name2');
   
-    addToCart.click(function () {
-      /* Creates a new list element */
-      const cartIcon = $('.addToCart');
+    addToCart1.click(function () {
+      const cartIcon1 = $('#cart1');
 
-      cartIcon.html(`
+      cartIcon1.html(`
       <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
       <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0"/>
       </svg>`);
-      console.log("Icon was clicked")
     });
+
+    addToCart2.click(function () {
+        const cartIcon2 = $('#cart2');
+  
+        cartIcon2.html(`
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0"/>
+        </svg>`);
+      });
   });
